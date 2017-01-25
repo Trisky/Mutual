e9f6a001ba96a87f2d4f923c408366a6e55b5a7c
Matías Miranda <matmiranda@live.com.ar>
2017-01-25 00:09:21 -0300

    Agrego una boludez en el script, mañana lo mejoro más

--- database_erase_create.sql
+++ database_erase_create.sql
@@ -1,20 +1,30 @@
 drop database mutual;
 create database mutual;
 use mutual;
-CREATE TABLE users (
-                    user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                    user_nombre VARCHAR(240) NOT NULL,
-                    user_password VARCHAR(240) NOT NULL
+
+CREATE TABLE tipo_usuario (
+					tipo_usuario_id INT(2) UNSIGNED PRIMARY KEY,
+                    tipo            VARCHAR(30) NOT NULL,
+                    descripcion     VARCHAR(240)
                     );
 
-
-
+CREATE TABLE users (
+                    user_id   INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    tipo_id   INT(2) UNSIGNED ,
+                    apellido  VARCHAR(50) NOT NULL,
+                    nombre    VARCHAR(50) NOT NULL,
+                    password  VARCHAR(16) NOT NULL,
+                    tipo_doc  VARCHAR(12) NOT NULL CHECK( tipo_doc in ('DNI', 'Pasaporte') ),
+                    num_doc   VARCHAR(12) NOT NULL,
+                    direccion VARCHAR(240),
+                    FOREIGN KEY (tipo_id) REFERENCES tipo_usuario(tipo_usuario_id)
+                   );
 
 CREATE TABLE prepaga (
                     prep_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                     prep_nombre VARCHAR(240) NOT NULL,
                     prep_precio DECIMAL NOT NULL
-                    
+
                     );
 
 
@@ -30,6 +40,8 @@ CREATE TABLE asociados (
 
 
 /* --inserto datos de prueba */
+INSERT INTO tipo_usuario( tipo_usuario_id, tipo, descripcion) VALUES (1, 'Developer', 'Desarrollador a cargo de soporte y mantenimiento'),
+																     (2, 'Admin', 'Administrador de la aplicación' );
 
 INSERT INTO users (user_nombre, user_password)
            VALUES ("admin", "sarasa");
